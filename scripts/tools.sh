9659fc8e0eb42bdf9b492fd5a3f57d30b0936d3f
mbianchif <89595589+mbianchif@users.noreply.github.com>
2025-05-04 20:03:48 -0300

    helix conf + taplo

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -32,6 +32,7 @@ sudo snap install typescript-language-server
 sudo snap install marksman
 sudo snap install bash-language-server --classic
 sudo snap install yaml-language-server
+cargo install taplo-cli --locked --features lsp
 
 echo "🌟 Installing Starship prompt..."
 curl -sS https://starship.rs/install.sh | sh -s -- -y
