f57cac9f3bef4391387abdd15e63a0a0e507d04d
mbianchif <89595589+mbianchif@users.noreply.github.com>
2025-05-06 00:28:59 -0300

    formatters

--- scripts/language-servers.sh
+++ scripts/language-servers.sh
@@ -4,7 +4,7 @@ echo "🧠 Installing language servers..."
 sudo apt install -y clangd
 rustup component add rust-analyzer
 cargo install taplo-cli --locked --features lsp
-pipx install python-lsp-server
+pipx install pyright black
 go install golang.org/x/tools/gopls@latest
 sudo snap install marksman
 sudo snap install bash-language-server --classic
