81e0ddbf8da8a1e91e0b4650ec2d20b4de0ddc27
mbianchif <89595589+mbianchif@users.noreply.github.com>
2025-05-20 14:13:35 -0300

    zellij: movement through tabs, lsp: marksman -> oxide

--- scripts/language-servers.sh
+++ scripts/language-servers.sh
@@ -6,6 +6,6 @@ rustup component add rust-analyzer
 cargo install taplo-cli --locked --features lsp
 pipx install pyright black
 go install golang.org/x/tools/gopls@latest
-sudo snap install marksman
+cargo install --locked --git https://github.com/Feel-ix-343/markdown-oxide.git markdown-oxide
 sudo snap install bash-language-server --classic
 sudo snap install yaml-language-server
