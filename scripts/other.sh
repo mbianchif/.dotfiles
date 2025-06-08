63ac175ff4e58fdfa0610cf610c1629852711482
mbianchif <89595589+mbianchif@users.noreply.github.com>
2025-06-08 18:12:08 -0300

    ghostty install through apt

--- scripts/other.sh
+++ scripts/other.sh
@@ -3,3 +3,4 @@
 echo "🌟 Installing others..."
 curl -sS https://starship.rs/install.sh | sh
 cargo install --locked zellij
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh)"
