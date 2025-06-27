c4cb8947de1d527ed71d504682a2c29c7ca7883c
mbianchif <89595589+mbianchif@users.noreply.github.com>
2025-06-27 01:36:00 -0300

    rm tmux

--- scripts/links.sh
+++ scripts/links.sh
@@ -11,5 +11,4 @@ ln -sf "$DOTFILES/starship" "$CONFIG"
 ln -sf "$DOTFILES/zellij"   "$CONFIG"
 
 ln -sf "$DOTFILES/.gitconfig" "$HOME"
-ln -sf "$DOTFILES/.tmux.conf" "$HOME"
 ln -sf "$DOTFILES/.bashrc"    "$HOME"
--- scripts/tools.sh
+++ scripts/tools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "🧰 Installing development tools..."
-sudo apt install -y build-essential curl ripgrep zip unzip btop bat tmux pipx
+sudo apt install -y build-essential curl ripgrep zip unzip btop bat pipx
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
