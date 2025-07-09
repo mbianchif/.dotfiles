db212d92b4c2bc05df552506a60d8df1c1976ebd
mbianchif <89595589+mbianchif@users.noreply.github.com>
2025-07-09 20:57:24 -0300

    added uv

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -2,4 +2,5 @@
 
 echo "🧰 Installing development tools..."
 sudo apt install -y build-essential curl ripgrep zip unzip btop bat pipx
+curl -LsSf https://astral.sh/uv/install.sh | sh
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
